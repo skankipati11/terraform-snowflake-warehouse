d8817cedaf77a9037803f1e3d9fbefd0a9a73ee2
skankipati11 <surya.kankipati11@gmail.com>
2026-01-22 16:09:36 +0530

    Changes

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -2,3 +2,10 @@ variable "context_templates" {
   description = "A map of context templates used to generate names"
   type        = map(string)
 }
+
+
+variable "snowflake_private_key" {
+  type        = string
+  description = "Private key used to access Snowflake"
+  sensitive   = true
+}
