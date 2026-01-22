d125b57e4c9f141e5ce4898f4dd0d7d3b7194f79
skankipati11 <surya.kankipati11@gmail.com>
2026-01-22 16:11:18 +0530

    Changes

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -10,3 +10,19 @@ provider "context" {
     environment = "dev"
   }
 }
+
+
+provider "snowflake" {
+
+  organization_name = "xodjopc"
+  account_name      = "jtc04659"
+  user              = "SKANKIPATI"
+  role              = "ACCOUNTADMIN"
+  authenticator     = "SNOWFLAKE_JWT"
+  private_key       = var.snowflake_private_key
+
+  preview_features_enabled = [
+    "snowflake_table_resource",
+    "snowflake_dynamic_table_resource"
+  ]
+}
